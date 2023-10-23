7f0abd19bd85d8e22f5378fc9c4a16e22e9fa580
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-23 12:42:20 +0300

    copy index_range function

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''pagination module'''
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    '''
+    Calculate the start and end indices for paginating a dataset.
+
+    Args:
+        page (int): The current page number (1-based).
+        page_size (int): The number of items per page.
+
+    Returns:
+        Tuple[int, int]: A tuple containing the start and end indices
+    '''
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
