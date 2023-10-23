a5d17ad8810378aa83bbdc3f8be3e31ccc340c7a
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-23 12:34:16 +0300

    type anotiate the function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
+from typing import Tuple
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     start = (page - 1) * page_size
     end = page + page_size
     return (start, end)
