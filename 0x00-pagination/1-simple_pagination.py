286c3db1d536935d249abe88af42ef9da3517eeb
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-23 12:42:57 +0300

    add server class

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 '''pagination module'''
 from typing import Tuple
+import csv
+import math
+from typing import List
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
@@ -17,3 +20,25 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     start = (page - 1) * page_size
     end = start + page_size
     return (start, end)
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+            pass
