ee0ba8a86a8a9bf54a1cd8745f88209250380ced
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-25 15:22:33 +0300

    change lfu logic

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -62,18 +62,22 @@ class LFUCache(BaseCaching):
         """
         if key is None or item is None:
             return
-
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                lfu_key, _ = self.keys_freq.pop()
+                lfu_key, _ = self.keys_freq[-1]
                 self.cache_data.pop(lfu_key)
+                self.keys_freq.pop()
                 print("DISCARD:", lfu_key)
-
             self.cache_data[key] = item
-            self.keys_freq.insert(0, (key, 0))
+            ins_index = len(self.keys_freq)
+            for i, key_freq in enumerate(self.keys_freq):
+                if key_freq[1] == 0:
+                    ins_index = i
+                    break
+            self.keys_freq.insert(ins_index, [key, 0])
         else:
             self.cache_data[key] = item
-            self.reorder_items_based_on_mru(key)
+            self.__reorder_items(key)
 
     def get(self, key):
         """Retrieve an item from the cache by its key.
