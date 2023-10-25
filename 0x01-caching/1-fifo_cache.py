d97b3d31a89328cfdb1e992a69bdc67e9b335102
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-25 10:41:52 +0300

    init values

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class FIFOCache(BaseCaching):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
