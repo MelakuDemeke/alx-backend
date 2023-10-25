92c8bb7a78b5849ab5ff5e2459dd26a94f5ee5d5
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-25 10:44:25 +0300

    add doc string to init

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -7,5 +7,6 @@ from collections import OrderedDict
 class FIFOCache(BaseCaching):
     """A caching mechanism that uses First-In First-Out (FIFO) strategy"""
     def __init__(self):
+        """Initialize the cache using an ordered dictionary"""
         super().__init__()
         self.cache_data = OrderedDict()
