546e8d2a6cadebed2f83bdbdc3c25a4efdb123c8
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-25 11:04:37 +0300

    add doc string to file and class

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+"""Last-In First-Out (LIFO) caching module."""
 
 from base_caching import BaseCaching
 
+
 class LIFOCache(BaseCaching):
+    """A caching mechanism that uses Last-In First-Out (LIFO) strategy."""
     pass
