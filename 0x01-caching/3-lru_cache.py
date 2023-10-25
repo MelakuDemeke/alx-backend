165d5a98c090823ff9cf2c26537587cff4811ca8
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-25 11:27:01 +0300

    init cache

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 """Least Recently Used (LRU) caching module.
 """
+from base_caching import BaseCaching
 from collections import OrderedDict
 
 class LRUCache(BaseCaching):
     """A caching mechanism that uses Least Recently Used (LRU) strategy.
     """
-    pass
+    def __init__(self):
+        """Initialize the cache using an ordered dictionary."""
+        super().__init__()
+        self.cache_data = OrderedDict()
