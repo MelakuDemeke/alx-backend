5c513a1b3dee24ce36d4a0ba59f610f2b68198fa
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-25 10:19:30 +0300

    add doc string in class and file

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
-
+'''
+This module defines the BasicCache class for basic caching using a dictionary
+'''
 from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
+    '''A class representing a basic cache.'''
     def put(self, key, item):
         if key is not None and item is not None:
             self.cache_data[key] = item
