7c02c58e5299dd5009bfaa6a8e81d170a0d8d415
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-25 11:29:58 +0300

    implement get

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -26,4 +26,6 @@ class LRUCache(BaseCaching):
         """Retrieve an item from the cache by key,
         updating its position as themost recently used.
         """
-        pass 
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None) 
