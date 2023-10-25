c303ed3fe04390b9846ef705e4d0d80d834f42c9
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-25 11:05:42 +0300

    init cache using orderd dict

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -2,8 +2,12 @@
 """Last-In First-Out (LIFO) caching module."""
 
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class LIFOCache(BaseCaching):
     """A caching mechanism that uses Last-In First-Out (LIFO) strategy."""
-    pass
+    def __init__(self):
+        """Initialize the cache using an ordered dictionary."""
+        super().__init__()
+        self.cache_data = OrderedDict()
