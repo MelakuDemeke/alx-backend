8acd8c66d3f0f2faaf38c677c79ac0d1305418c8
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-25 10:43:53 +0300

    add doc string to calss and file

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-
+"""First-In First-Out (FIFO) caching module."""
 from base_caching import BaseCaching
 from collections import OrderedDict
 
 
 class FIFOCache(BaseCaching):
+    """A caching mechanism that uses First-In First-Out (FIFO) strategy"""
     def __init__(self):
         super().__init__()
         self.cache_data = OrderedDict()
