2a90c23d9f8b9d3fdea8d1ff36968db064cc1cff
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-25 10:18:44 +0300

    def get function taht fetch cachedata with a key

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -6,3 +6,6 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         if key is not None and item is not None:
             self.cache_data[key] = item
+
+    def get(self, key):
+        return self.cache_data.get(key, None)
