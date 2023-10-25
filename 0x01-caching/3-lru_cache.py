99694c9f8229ca99891ed98af36bf988d6378fd1
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-25 11:38:49 +0300

    expand logic

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -16,12 +16,16 @@ class LRUCache(BaseCaching):
     def put(self, key, item):
         """Add an item to the cache, implementing LRU if the limit is reached.
         """
-        if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-            lru_key, _ = self.cache_data.popitem(True)
-            print("DISCARD:", lru_key)
-
-        self.cache_data[key] = item
-        self.cache_data.move_to_end(key, last=False)
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                lru_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", lru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
         """Retrieve an item from the cache by key,
