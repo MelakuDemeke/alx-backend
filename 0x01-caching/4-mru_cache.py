777830825fedde390d61cda9a258a3c25bd1bde1
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-25 14:05:08 +0300

    add doc string

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -3,6 +3,10 @@
 from base_caching import BaseCaching
 
 class MRUCache(BaseCaching):
+    """
+    Represents a Most Recently Used (MRU) caching mechanism.
+    Inherits from BaseCaching.
+    """
     def __init__(self):
         """Initializes the cache with an empty OrderedDict."""
         super().__init__()
