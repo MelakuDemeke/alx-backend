ade9c0d7325df6dafb5c89ba810ada3cab03ab7f
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-25 14:06:40 +0300

    implement mru

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -21,3 +21,10 @@ class MRUCache(BaseCaching):
         """
         if key is None or item is None:
             return
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                mru_key, _ = self.cache_data.popitem(last=False)
+                print("DISCARD:", mru_key)
+            self.cache_data[key] = item
