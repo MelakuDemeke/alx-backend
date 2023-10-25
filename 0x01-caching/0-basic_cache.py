6014d8f641a329ff49a033315182f35a71811f67
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-25 10:21:58 +0300

    pycode style fix

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -4,6 +4,7 @@ This module defines the BasicCache class for basic caching using a dictionary
 '''
 from base_caching import BaseCaching
 
+
 class BasicCache(BaseCaching):
     '''A class representing a basic cache.'''
     def put(self, key, item):
