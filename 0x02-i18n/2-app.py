26f51b5ac376c94386ec3e4937c6cda627c6e456
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-31 12:23:02 +0300

    add locale selector

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -14,11 +14,15 @@ app.config['BABEL_DEFAULT_LOCALE'] = 'en'
 app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
 babel = Babel(app)
 
+@babel.localeselector
+def get_locale():
+    """Get locale language from request"""
+    return request.accept_languages.best_match(app.config_class.LANGUAGES)
 
 @app.route('/')
 def index():
     """index page of the app"""
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
