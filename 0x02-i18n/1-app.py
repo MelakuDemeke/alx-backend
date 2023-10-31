7405536b9ff81b5490b4315b73ac2220e9de354b
Melaku <MelakuDemeke>
2023-10-31 21:06:56 +0300

    pycode style fix

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -3,21 +3,25 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 
+
 class Config:
     """Create a Flask Babel configuration."""
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
-app.config_class = Config
-app.config['LANGUAGES'] = Config.LANGUAGES
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
-app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
+
 
 @app.route('/')
 def index():
     """index page of the app"""
     return render_template('1-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
