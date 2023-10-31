0ec6a8d9ba4958ffe49bf13accd12b647283391d
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-31 12:14:37 +0300

    initialize the files

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Flask app to server hello world"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """index page of the app"""
+    return render_template('1-index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
