0ddc78515890d1b0105a82f94bc8a5cfa0759484
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-31 15:14:31 +0300

    force with url parameter

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -16,12 +16,22 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale():
     """Get locale language from request"""
+    queries = request.query_string.decode('utf-8').split('&')
+    query_table = dict(map(
+        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
+        queries,
+    ))
+    if 'locale' in query_table:
+        if query_table['locale'] in app.config["LANGUAGES"]:
+            return query_table['locale']
     return request.accept_languages.best_match(app.config["LANGUAGES"])
+
 
 @app.route('/')
 def index():
     """index page of the app"""
-    return render_template('3-index.html')
+    return render_template('4-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
