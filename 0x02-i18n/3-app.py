14a2d8a2c20ff77c10f9fb8b78b1f550b3c1097c
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-31 12:28:19 +0300

    initial files

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Flask app to server hello world"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+class Config:
+    """Create a Flask Babel configuration."""
+    LANGUAGES = ["en", "fr"]
+
+app = Flask(__name__)
+app.config_class = Config
+app.config['LANGUAGES'] = Config.LANGUAGES
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    """Get locale language from request"""
+    return request.accept_languages.best_match(app.config_class.LANGUAGES)
+
+@app.route('/')
+def index():
+    """index page of the app"""
+    return render_template('2-index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
