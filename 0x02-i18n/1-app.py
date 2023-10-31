1cf26306f3d0f107256e8d7007c963313a6e2c5c
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-31 12:17:55 +0300

    create the config class using babel

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 """Flask app to server hello world"""
 from flask import Flask, render_template
+from flask_babel import Babel
+
+class Config:
+    """Create a Flask Babel configuration."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 app = Flask(__name__)
 
