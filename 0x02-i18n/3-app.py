1127aca98a96beec52ddf7d9ae101014fbe045ab
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-31 15:08:42 +0300

    change confi

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -6,18 +6,17 @@ from flask_babel import Babel
 class Config:
     """Create a Flask Babel configuration."""
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 app = Flask(__name__)
-app.config_class = Config
-app.config['LANGUAGES'] = Config.LANGUAGES
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
-app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+app.config.from_object(Config)
 babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
     """Get locale language from request"""
-    return request.accept_languages.best_match(app.config_class.LANGUAGES)
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 @app.route('/')
 def index():
