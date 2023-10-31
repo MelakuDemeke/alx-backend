34a39c02be33b5420dd10d7d0d316fe87f76e4e1
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-31 15:19:24 +0300

    login

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,36 +1,66 @@
 #!/usr/bin/env python3
 """Flask app to server hello world"""
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
+
 class Config:
-    """Create a Flask Babel configuration."""
+    """Represents a Flask Babel configuration.
+    """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config_class = Config
+app.config['LANGUAGES'] = Config.LANGUAGES
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+app.url_map.strict_slashes = False
 babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user(id=None):
+    """Retrieves a user based on a user id.
+    """
+    try:
+        return users.get(int(id), None)
+    except Exception:
+        return None
+
+
+@app.before_request
+def before_request():
+    queries = request.query_string.decode('utf-8').split('&')
+    query_table = dict(map(lambda x: x.split('='), queries))
+    login_id = query_table.get('login_as', '')
+    user = get_user(login_id)
+    setattr(g, 'user', user)
 
 @babel.localeselector
 def get_locale():
     """Get locale language from request"""
     queries = request.query_string.decode('utf-8').split('&')
-    query_table = dict(map(
-        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
-        queries,
-    ))
-    if 'locale' in query_table:
-        if query_table['locale'] in app.config["LANGUAGES"]:
-            return query_table['locale']
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+    query_table = dict(map(lambda x: x.split('='), queries))
+    locale = query_table.get('locale', '')
+    if locale in app.config_class.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(app.config_class.LANGUAGES)
 
 
 @app.route('/')
 def index():
     """index page of the app"""
-    return render_template('4-index.html')
+    user_details = getattr(g, 'user', None)
+    ctxt = {
+        'login_details': user_details,
+    }
+    return render_template('5-index.html', **ctxt)
 
 
 if __name__ == '__main__':
