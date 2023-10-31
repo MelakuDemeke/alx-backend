6effc97587617d5039c8ac64f74667a327130687
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-31 12:22:47 +0300

    starter

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Flask app to server hello world"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+class Config:
+    """Create a Flask Babel configuration."""
+    LANGUAGES = ["en", "fr"]
+
+app = Flask(__name__)
+app.config_class = Config
+app.config['LANGUAGES'] = Config.LANGUAGES
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """index page of the app"""
+    return render_template('1-index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
