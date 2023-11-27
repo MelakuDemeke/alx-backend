b061d2ae1720ebdb849f7756260925e088f92c3d
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-27 16:40:07 +0300

    Add variables for total and pending notifications

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -4,5 +4,7 @@ const queue = createQueue();
 const BLACKLISTED_NUMBERS = ['4153518780', '4153518781'];
 
 const sendNotification = (phoneNumber, message, job, done) => {
+  let totalNotifications = 2;
+  let pendingNotifications = 2;
   
 }
