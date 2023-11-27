3618746dd24812ce55cee6e817816cab9218890a
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-27 15:54:32 +0300

    Fix formatting and add job creation for push notification

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,3 +1,13 @@
 import { createQueue } from 'kue';
 
-const queue = createQueue({name: 'push_notification_code'});
+const queue = createQueue({ name: 'push_notification_code' });
+
+const job = queue.create('push_notification_code', {
+  phoneNumber: '0102030405',
+  message: 'Account registered',
+});
+
+job
+  .on('enqueue', () => {
+    console.log('Notification job created:', job.id);
+  })
