5512b17be7f639268fe01ac89ad53561d990b0fe
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-27 15:38:02 +0300

    Add course start messages and kill server message

--- 0x03-queuing_system_in_js/5-publisher.js
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -16,3 +16,8 @@ const publishMessage = (message, time) => {
     client.publish('holberton school channel', message);
   }, time);
 };
+
+publishMessage("Holberton Student #1 starts course", 100);
+publishMessage("Holberton Student #2 starts course", 200);
+publishMessage("KILL_SERVER", 300);
+publishMessage("Holberton Student #3 starts course", 400);
