d73a406f1df1f36491f3fb24f26c86c79f894bad
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-27 16:02:54 +0300

    create jobs array

---
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -0,0 +1,46 @@
+const jobs = [
+	{
+		phoneNumber: '4153518780',
+		message: 'This is the code 1234 to verify your account'
+	},
+	{
+		phoneNumber: '4153518781',
+		message: 'This is the code 4562 to verify your account'
+	},
+	{
+		phoneNumber: '4153518743',
+		message: 'This is the code 4321 to verify your account'
+	},
+	{
+		phoneNumber: '4153538781',
+		message: 'This is the code 4562 to verify your account'
+	},
+	{
+		phoneNumber: '4153118782',
+		message: 'This is the code 4321 to verify your account'
+	},
+	{
+		phoneNumber: '4153718781',
+		message: 'This is the code 4562 to verify your account'
+	},
+	{
+		phoneNumber: '4159518782',
+		message: 'This is the code 4321 to verify your account'
+	},
+	{
+		phoneNumber: '4158718781',
+		message: 'This is the code 4562 to verify your account'
+	},
+	{
+		phoneNumber: '4153818782',
+		message: 'This is the code 4321 to verify your account'
+	},
+	{
+		phoneNumber: '4154318781',
+		message: 'This is the code 4562 to verify your account'
+	},
+	{
+		phoneNumber: '4151218782',
+		message: 'This is the code 4321 to verify your account'
+	}
+];
