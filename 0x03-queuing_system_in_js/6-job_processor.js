50d2f19fc5a2f861d9405f8d847b32eac6b460a2
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-27 15:57:32 +0300

    Add sendNotification function to job processor

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -1,3 +1,11 @@
 import { createQueue } from 'kue';
 
 const queue = createQueue();
+
+const sendNotification = (phoneNumber, message) => {
+  console.log(
+    `Sending notification to ${phoneNumber},`,
+    'with message:',
+    message,
+  );
+};
