b1126b8269b10bfb2aed326e4c1bd3fce847b06d
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-27 16:40:34 +0300

    Fix job progress calculation in sendNotification function

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -6,5 +6,10 @@ const BLACKLISTED_NUMBERS = ['4153518780', '4153518781'];
 const sendNotification = (phoneNumber, message, job, done) => {
   let totalNotifications = 2;
   let pendingNotifications = 2;
-  
+  let sendInterval = setInterval(() => {
+    if (totalNotifications - pendingNotifications <= totalNotifications / 2) {
+      job.progress(totalNotifications - pendingNotifications, totalNotifications);
+    }
+
+  }, 1000);
 }
