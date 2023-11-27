429e739da113d2f55d0609fdde5d3c04c835eaef
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-27 15:35:32 +0300

    Add message handling for unsubscribe and quit

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -11,3 +11,11 @@ client.on('connect', () => {
 });
 
 client.subscribe('holberton school channel');
+
+client.on('message', (_err, msg) => {
+  console.log(msg);
+  if (msg === EXIT_MSG) {
+    client.unsubscribe();
+    client.quit();
+  }
+});
