e2100e909bf51df84c9f09e276ce2f3ab36d7a28
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-27 16:01:44 +0300

    change message & phone n0 to be similar as example

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -3,8 +3,8 @@ import { createQueue } from 'kue';
 const queue = createQueue({ name: 'push_notification_code' });
 
 const job = queue.create('push_notification_code', {
-  phoneNumber: '0102030405',
-  message: 'Account registered',
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account',
 });
 
 job
