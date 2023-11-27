61809d7e0896006a71b57e63801e5024d57fdf89
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-27 15:06:08 +0300

    Add city population data to main function

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -15,7 +15,14 @@ const printHash = (hashName) => {
 };
 
 function main() {
-
+  const city = {
+    Portland: 50,
+    Seattle: 80,
+    'New York': 20,
+    Bogota: 20,
+    Cali: 40,
+    Paris: 2,
+  };
 }
 
 client.on('connect', () => {
