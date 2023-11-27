d3c250f32293464ebf30568977319028ae5d5936
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-27 15:33:43 +0300

    Add error handling for Redis client connection

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,3 +1,7 @@
 import { createClient, print } from 'redis';
 
 const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
