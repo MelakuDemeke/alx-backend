7f33cb37c876e085831f3f443e3fcebc37782504
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-27 16:23:01 +0300

    Add sendNotification function

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -2,3 +2,7 @@ import { createQueue, Job } from 'kue';
 
 const queue = createQueue();
 const BLACKLISTED_NUMBERS = ['4153518780', '4153518781'];
+
+const sendNotification = (phoneNumber, message, job, done) => {
+  
+}
