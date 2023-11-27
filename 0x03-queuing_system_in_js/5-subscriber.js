55dad1d47ec40cf188fd850eccbd6ffac7c1cbe6
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-27 15:34:52 +0300

    Add Redis client connection event listener

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -5,3 +5,7 @@ const client = createClient();
 client.on('error', (err) => {
   console.log('Redis client not connected to the server:', err.toString());
 });
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
