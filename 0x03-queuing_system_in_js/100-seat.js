9c25faa2be8513fdf83f3a6b440bad45dd983e62
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-28 12:04:51 +0300

    Add reserveSeat function to set available seats

--- 0x03-queuing_system_in_js/100-seat.js
+++ 0x03-queuing_system_in_js/100-seat.js
@@ -1,3 +1,7 @@
 import { createClient } from 'redis';
 
 const client = createClient();
+
+const reserveSeat = async (number) => {
+  return promisify(client.SET).bind(client)('available_seats', number);
+};
