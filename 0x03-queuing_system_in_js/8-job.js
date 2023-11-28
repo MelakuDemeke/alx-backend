d369dc1922aa4e7b957f5582022083fc9e56a75f
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-28 11:07:45 +0300

    Add push notification jobs to queue

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -4,4 +4,7 @@ export const createPushNotificationsJobs = (jobs, queue) => {
   if (!(jobs instanceof Array)) {
     throw new Error('Jobs is not an array');
   }
+  for (const singleJob of jobs) {
+    const job = queue.create('push_notification_code_3', singleJob);
+  }
 }
