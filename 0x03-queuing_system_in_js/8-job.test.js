23b310e367bdc4a86a5bb8e63bb98c0df180c499
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-28 11:12:00 +0300

    Add test for createPushNotificationsJobs function

--- 0x03-queuing_system_in_js/8-job.test.js
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -3,4 +3,6 @@ import { expect } from 'chai';
 import { createQueue } from 'kue';
 import createPushNotificationsJobs from './8-job.js';
 
-
+describe('createPushNotificationsJobs', () => {
+  
+});
