eb186a4b6e724c34747ff0c455c82bddb889f786
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-28 12:05:50 +0300

    Add initial seats count and reservation status

--- 0x03-queuing_system_in_js/100-seat.js
+++ 0x03-queuing_system_in_js/100-seat.js
@@ -1,6 +1,8 @@
 import { createClient } from 'redis';
 
 const client = createClient();
+const INITIAL_SEATS_COUNT = 50;
+let reservationEnabled = false;
 
 const reserveSeat = async (number) => {
   return promisify(client.SET).bind(client)('available_seats', number);
