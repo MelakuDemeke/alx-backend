7713813f537427f79dec14767e503442ce44cd48
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-28 12:09:10 +0300

    Add queue functionality to seat reservation

--- 0x03-queuing_system_in_js/100-seat.js
+++ 0x03-queuing_system_in_js/100-seat.js
@@ -1,8 +1,10 @@
 import { createClient } from 'redis';
+import { createQueue } from 'kue';
 
-const client = createClient();
+const client = createClient({ name: 'reserve_seat' });
 const INITIAL_SEATS_COUNT = 50;
 let reservationEnabled = false;
+const queue = createQueue();
 
 const reserveSeat = async (number) => {
   return promisify(client.SET).bind(client)('available_seats', number);
