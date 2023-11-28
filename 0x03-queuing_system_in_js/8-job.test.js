f5a8c363ecd00d3cf3728af47884b9ce96c78e74
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-28 11:14:34 +0300

    Update import statement in createPushNotificationsJobs test

--- 0x03-queuing_system_in_js/8-job.test.js
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -1,8 +1,9 @@
 import sinon from 'sinon';
 import { expect } from 'chai';
-import { createQueue } from 'kue';
+import { createpushNotificationQ } from 'kue';
 import createPushNotificationsJobs from './8-job.js';
 
 describe('createPushNotificationsJobs', () => {
-  
+  const consoleSpy = sinon.spy(console);
+  const pushNotificationQ = createpushNotificationQ({ name: 'push_notification_code_test' });
 });
