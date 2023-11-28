3328245bf35535586c474ecd85d881aacaf7b366
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-28 11:08:27 +0300

    Add event listener for job failure in createPushNotificationsJobs function

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -13,5 +13,8 @@ export const createPushNotificationsJobs = (jobs, queue) => {
     .on('complete', () => {
       console.log('Notification job', job.id, 'completed');
     })
+    .on('failed', (err) => {
+      console.log('Notification job', job.id, 'failed:', err.message || err.toString());
+    })
   }
 }
