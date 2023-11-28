d7b4a0a2185afe9e9e87c703a56be875667a3d92
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-28 12:05:12 +0300

    Add getCurrentAvailableSeats function

--- 0x03-queuing_system_in_js/100-seat.js
+++ 0x03-queuing_system_in_js/100-seat.js
@@ -5,3 +5,8 @@ const client = createClient();
 const reserveSeat = async (number) => {
   return promisify(client.SET).bind(client)('available_seats', number);
 };
+
+const getCurrentAvailableSeats = async () => {
+  return promisify(client.GET).bind(client)('available_seats');
+};
+
