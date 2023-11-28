93da2d8883f285537131f0ca917902cb6c160ffc
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-28 11:07:05 +0300

    Add error handling for non-array jobs

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -1,5 +1,7 @@
 import { Queue, Job } from 'kue';
 
 export const createPushNotificationsJobs = (jobs, queue) => {
-  
+  if (!(jobs instanceof Array)) {
+    throw new Error('Jobs is not an array');
+  }
 }
